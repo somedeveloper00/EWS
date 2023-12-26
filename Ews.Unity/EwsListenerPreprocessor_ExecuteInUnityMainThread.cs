071b13b85eeb4ce6f3d638171fc449100337180b
Saeed Barari <barari.saeed.am@outlook.com>
2023-12-27 00:30:30 +0330

    Refactored EwsListenerPreprocessor_ExecuteInUnityMainThread.cs

--- Ews.Unity/EwsListenerPreprocessor_ExecuteInUnityMainThread.cs
+++ Ews.Unity/EwsListenerPreprocessor_ExecuteInUnityMainThread.cs
@@ -15,16 +15,16 @@ namespace EWS.Unity
         public void ExecuteNewEvent(EwsClient client, byte[] message, IEwsEventListener listener)
         {
             _queue.Enqueue(() => listener.Process(client, message));
-            enabled = true;
+            Debug.Log("new queue");
         }
 
         private void Update()
         {
             while (_queue.TryDequeue(out var action))
             {
+                Debug.Log("dequeued");
                 action?.Invoke();
             }
-            enabled = false;
         }
     }
 }
